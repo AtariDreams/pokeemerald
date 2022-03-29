4d40927c6f3ed03302132aca529577bbfcc47d6e
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-03-29 16:02:50 -0400

    WTF

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -1222,7 +1222,7 @@ bool32 GetTrainerFlagFromScriptPointer(const u8 *data)
 void SetTrainerFacingDirection(void)
 {
     struct ObjectEvent *objectEvent = &gObjectEvents[gSelectedObjectEvent];
-    SetTrainerMovementType(objectEvent, GetTrainerFacingDirectionMovementType(objectEvent->facingDirection));
+    SetTrainerMovementType(objectEvent, GetTrainerFacingDirectionMovementType(objectEvent->directions.regDir.facingDirection));
 }
 
 u8 GetTrainerBattleMode(void)
