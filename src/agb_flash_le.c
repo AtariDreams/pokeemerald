57dd82691c2bbfff57df108fe37c40564c505eab
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-04-12 15:44:35 -0400

    Epic MATCH

--- src/agb_flash_le.c
+++ src/agb_flash_le.c
@@ -1,12 +1,12 @@
 #include "gba/gba.h"
 #include "gba/flash_internal.h"
 
-const u16 leMaxTime[] =
+static const u16 leMaxTime[][3] =
 {
-      10, 65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK,
-      10, 65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK,
-    2000, 65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK,
-    2000, 65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK,
+      {10, 65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK},
+      {10, 65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK},
+    {2000, 65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK},
+    {2000, 65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK},
 };
 
 const struct FlashSetupInfo LE26FV10N1TS =
@@ -18,7 +18,7 @@ const struct FlashSetupInfo LE26FV10N1TS =
     WaitForFlashWrite_Common,
     leMaxTime,
     {
-        131072, // ROM size
+        0x20000, // ROM size
         {
             4096, // sector size
               12, // bit shift to multiply by sector size (4096 == 1 << 12)
