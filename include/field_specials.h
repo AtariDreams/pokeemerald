2b2a22c2c4a59a6b38fd6a26d8819e6c2bd8ba4e
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-08-06 12:05:50 -0400

    OK

--- include/field_specials.h
+++ include/field_specials.h
@@ -18,12 +18,14 @@ bool32 ShouldDoScottBattleFrontierCall(void);
 bool32 ShouldDoRoxanneCall(void);
 bool32 ShouldDoRivalRayquazaCall(void);
 bool32 CountSSTidalStep(u16 delta);
-u8 GetSSTidalLocation(s8 *mapGroup, s8 *mapNum, s16 *x, s16 *y);
+u8 GetSSTidalLocation(u8 *mapGroup, u8 *mapNum, s16 *x, s16 *y);
 void ShowScrollableMultichoice(void);
 void FrontierGamblerSetWonOrLost(bool8 won);
 u8 TryGainNewFanFromCounter(u8 incrementId);
 bool8 InPokemonCenter(void);
+#if !MODERN
 void SetShoalItemFlag(u16 unused);
+#endif
 void UpdateFrontierManiac(u16 daysSince);
 void UpdateFrontierGambler(u16 daysSince);
 void ResetCyclingRoadChallengeData(void);
