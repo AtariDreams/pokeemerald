1e5ffee5d0fe243a373e1b27d9852ce81eab0395
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-08-06 12:05:58 -0400

    OKAY

--- src/record_mixing.c
+++ src/record_mixing.c
@@ -773,9 +773,11 @@ static void ReceiveDaycareMailData(struct RecordMixingDaycareMail *records, size
     struct RecordMixingDaycareMail *mixMail;
     u8 playerSlot1, playerSlot2;
     void *ptr;
+    #if !MODERN
     u8 unusedArr1[MAX_LINK_PLAYERS];
     u8 unusedArr2[MAX_LINK_PLAYERS];
     struct RecordMixingDaycareMail *unusedMixMail[MAX_LINK_PLAYERS];
+    #endif
     bool8 canHoldItem[MAX_LINK_PLAYERS][DAYCARE_MON_COUNT];
     u8 idxs[MAX_LINK_PLAYERS][2];
     u8 numDaycareCanHold;
@@ -790,8 +792,10 @@ static void ReceiveDaycareMailData(struct RecordMixingDaycareMail *records, size
     linkPlayerCount = GetLinkPlayerCount();
     for (i = 0; i < MAX_LINK_PLAYERS; i++)
     {
+        #if !MODERN
         unusedArr1[i] = 0xFF;
         unusedArr2[i] = 0;
+        #endif
         canHoldItem[i][0] = FALSE;
         canHoldItem[i][1] = FALSE;
     }
@@ -928,12 +932,14 @@ static void ReceiveDaycareMailData(struct RecordMixingDaycareMail *records, size
         }
     }
 
+    #if !MODERN
     // Copy the player's record mix mail 4 times to an array that's never read.
     for (i = 0; i < MAX_LINK_PLAYERS; i++)
     {
         mixMail = &records[multiplayerId * recordSize];
         unusedMixMail[i] = mixMail;
     }
+    #endif
 
     // Choose a random table id to determine who will
     // swap if there are more than 2 candidate players.
