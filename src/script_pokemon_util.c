bb39206ae4e3e70faf3d4b30c5c51532be2834d3
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-08-17 14:39:59 -0400

    OK

--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -23,6 +23,18 @@
 #include "tv.h"
 #include "constants/items.h"
 #include "constants/battle_frontier.h"
+#include "battle_pike.h"
+#include "battle_pyramid.h"
+#include "field_message_box.h"
+#include "field_poison.h"
+#include "fldeff_misc.h"
+#include "frontier_util.h"
+#include "pokenav.h"
+#include "strings.h"
+#include "task.h"
+#include "trainer_hill.h"
+#include "constants/field_poison.h"
+#include "constants/party_menu.h"
 
 static void CB2_ReturnFromChooseHalfParty(void);
 static void CB2_ReturnFromChooseBattleFrontierParty(void);
@@ -175,14 +187,13 @@ void ChooseHalfPartyForBattle(void)
 
 static void CB2_ReturnFromChooseHalfParty(void)
 {
-    switch (gSelectedOrderFromParty[0])
+    if (gSelectedOrderFromParty[0] == 0)
     {
-    case 0:
         gSpecialVar_Result = FALSE;
-        break;
-    default:
+    }
+    else
+    {
         gSpecialVar_Result = TRUE;
-        break;
     }
 
     SetMainCallback2(CB2_ReturnToFieldContinueScriptPlayMapMusic);
@@ -196,14 +207,13 @@ void ChoosePartyForBattleFrontier(void)
 
 static void CB2_ReturnFromChooseBattleFrontierParty(void)
 {
-    switch (gSelectedOrderFromParty[0])
+    if (gSelectedOrderFromParty[0] == 0)
     {
-    case 0:
         gSpecialVar_Result = FALSE;
-        break;
-    default:
+    }
+    else
+    {
         gSpecialVar_Result = TRUE;
-        break;
     }
 
     SetMainCallback2(CB2_ReturnToFieldContinueScriptPlayMapMusic);
@@ -228,4 +238,136 @@ void ReducePlayerPartyToSelectedMons(void)
         gPlayerParty[i] = party[i];
 
     CalculatePlayerPartyCount();
+}
+
+static bool32 IsMonValidSpecies(struct Pokemon *pokemon)
+{
+    u16 species = GetMonData(pokemon, MON_DATA_SPECIES2);
+    if (species == SPECIES_NONE || species == SPECIES_EGG)
+        return FALSE;
+
+    return TRUE;
+}
+
+static bool32 AllMonsFainted(void)
+{
+    int i;
+    struct Pokemon *pokemon = gPlayerParty;
+
+    for (i = 0; i < PARTY_SIZE; i++, pokemon++)
+    {
+        if (IsMonValidSpecies(pokemon) && GetMonData(pokemon, MON_DATA_HP) != 0)
+            return FALSE;
+    }
+    return TRUE;
+}
+
+static void FaintFromFieldPoison(u8 partyIdx)
+{
+    struct Pokemon *pokemon = &gPlayerParty[partyIdx];
+    u32 status = STATUS1_NONE;
+
+    AdjustFriendship(pokemon, FRIENDSHIP_EVENT_FAINT_FIELD_PSN);
+    SetMonData(pokemon, MON_DATA_STATUS, &status);
+    GetMonData(pokemon, MON_DATA_NICKNAME, gStringVar1);
+    StringGet_Nickname(gStringVar1);
+}
+
+static bool32 MonFaintedFromPoison(u8 partyIdx)
+{
+    struct Pokemon *pokemon = &gPlayerParty[partyIdx];
+    if (IsMonValidSpecies(pokemon) && GetMonData(pokemon, MON_DATA_HP) == 0 && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN)
+        return TRUE;
+
+    return FALSE;
+}
+
+#define tState    data[0]
+#define tPartyIdx data[1]
+
+static void Task_TryFieldPoisonWhiteOut(u8 taskId)
+{
+    s16 *data = gTasks[taskId].data;
+    switch (tState)
+    {
+    case 0:
+        for (; tPartyIdx < PARTY_SIZE; tPartyIdx++)
+        {
+            if (MonFaintedFromPoison(tPartyIdx))
+            {
+                FaintFromFieldPoison(tPartyIdx);
+                ShowFieldMessage(gText_PkmnFainted_FldPsn);
+                tState++;
+                return;
+            }
+        }
+        tState = 2; // Finished checking party
+        break;
+    case 1:
+        // Wait for "{mon} fainted" message, then return to party loop
+        if (IsFieldMessageBoxHidden())
+            tState--;
+        break;
+    case 2:
+        if (AllMonsFainted())
+        {
+            // Battle facilities have their own white out script to handle the challenge loss
+            if (InBattlePyramid() | InBattlePike() || InTrainerHillChallenge())
+                gSpecialVar_Result = FLDPSN_FRONTIER_WHITEOUT;
+            else
+                gSpecialVar_Result = FLDPSN_WHITEOUT;
+        }
+        else
+        {
+            gSpecialVar_Result = FLDPSN_NO_WHITEOUT;
+        }
+        ScriptContext_Enable();
+        DestroyTask(taskId);
+        break;
+    }
+}
+
+#undef tState
+#undef tPartyIdx
+
+void TryFieldPoisonWhiteOut(void)
+{
+    CreateTask(Task_TryFieldPoisonWhiteOut, 80);
+    ScriptContext_Stop();
+}
+
+s32 DoPoisonFieldEffect(void)
+{
+    int i;
+    u32 hp;
+    struct Pokemon *pokemon = gPlayerParty;
+    u32 numPoisoned = 0;
+    u32 numFainted = 0;
+
+    for (i = 0; i < PARTY_SIZE; i++)
+    {
+        if (GetMonData(pokemon, MON_DATA_SANITY_HAS_SPECIES) && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN)
+        {
+            // Apply poison damage
+            hp = GetMonData(pokemon, MON_DATA_HP);
+            if (hp == 0 || --hp == 0)
+                numFainted++;
+
+            SetMonData(pokemon, MON_DATA_HP, &hp);
+            numPoisoned++;
+        }
+        pokemon++;
+    }
+
+    // Do screen flash effect
+    if (numFainted != 0 || numPoisoned != 0)
+        FldEffPoison_Start();
+
+    if (numFainted != 0)
+        return FLDPSN_FNT;
+
+    if (numPoisoned != 0)
+        return FLDPSN_PSN;
+
+    return FLDPSN_NONE;
 }
