6bfbe838f69f7675dca0068168e06900087cc5d6
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-08-17 14:42:02 -0400

    OK

--- gflib/window.c
+++ gflib/window.c
@@ -6,9 +6,9 @@
 
 // This global is set to 0 and never changed.
 #if !MODERN
-u8 gTransparentTileNumber;
 u32 gUnusedWindowVar1;
 u32 gUnusedWindowVar2;
+u8 gTransparentTileNumber;
 u32 gUnusedWindowVar3;
 #endif
 void *gWindowBgTilemapBuffers[NUM_BACKGROUNDS];
