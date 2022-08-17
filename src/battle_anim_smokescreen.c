732c6566f89b96f36c3dde1432640d4a4ae8b295
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-08-17 14:39:11 -0400

    Epic

--- src/battle_anim_smokescreen.c
+++ src/battle_anim_smokescreen.c
@@ -165,8 +165,7 @@ const struct SpriteTemplate gSpriteTemplate_EnemyShadow =
 u8 SmokescreenImpact(s16 x, s16 y, bool8 persist)
 {
     u8 mainSpriteId;
-    u8 spriteId1, spriteId2, spriteId3, spriteId4;
-    struct Sprite *mainSprite;
+    u8 spriteId1;
 
     if (GetSpriteTileStartByTag(sSmokescreenImpactSpriteSheet.tag) == 0xFFFF)
     {
@@ -175,57 +174,31 @@ u8 SmokescreenImpact(s16 x, s16 y, bool8 persist)
     }
 
     mainSpriteId = CreateInvisibleSpriteWithCallback(SpriteCB_SmokescreenImpactMain);
-    mainSprite = &gSprites[mainSpriteId];
-    mainSprite->sPersist = persist;
+    gSprites[mainSpriteId].sPersist = persist;
 
     // Top left sprite
     spriteId1 = CreateSprite(&sSmokescreenImpactSpriteTemplate, x - 16, y - 16, 2);
-    gSprites[spriteId1].sMainSpriteId = mainSpriteId;
-    mainSprite->sActiveSprites++;
+    gSprites[mainSpriteId].sActiveSprites++;
     AnimateSprite(&gSprites[spriteId1]);
 
     // Top right sprite
-    spriteId2 = CreateSprite(&sSmokescreenImpactSpriteTemplate, x, y - 16, 2);
-    gSprites[spriteId2].sMainSpriteId = mainSpriteId;
-    mainSprite->sActiveSprites++;
-    StartSpriteAnim(&gSprites[spriteId2], 1);
-    AnimateSprite(&gSprites[spriteId2]);
+    spriteId1 = CreateSprite(&sSmokescreenImpactSpriteTemplate, x, y - 16, 2);
+    gSprites[spriteId1].sMainSpriteId = mainSpriteId;
+    gSprites[mainSpriteId].sActiveSprites++;
+    StartSpriteAnim(&gSprites[spriteId1], 1);
+    AnimateSprite(&gSprites[spriteId1]);
 
     // Bottom left sprite
-    spriteId3 = CreateSprite(&sSmokescreenImpactSpriteTemplate, x - 16, y, 2);
-    gSprites[spriteId3].sMainSpriteId = mainSpriteId;
-    mainSprite->sActiveSprites++;
-    StartSpriteAnim(&gSprites[spriteId3], 2);
-    AnimateSprite(&gSprites[spriteId3]);
+    spriteId1 = CreateSprite(&sSmokescreenImpactSpriteTemplate, x - 16, y, 2);
+    gSprites[spriteId1].sMainSpriteId = mainSpriteId;
+    gSprites[mainSpriteId].sActiveSprites++;
+    StartSpriteAnim(&gSprites[spriteId1], 2);
+    AnimateSprite(&gSprites[spriteId1]);
 
     // Bottom right sprite
-    spriteId4 = CreateSprite(&sSmokescreenImpactSpriteTemplate, x, y, 2);
-    gSprites[spriteId4].sMainSpriteId = mainSpriteId;
-    mainSprite->sActiveSprites++;
-    StartSpriteAnim(&gSprites[spriteId4], 3);
-    AnimateSprite(&gSprites[spriteId4]);
-
-    return mainSpriteId;
-}
-
-static void SpriteCB_SmokescreenImpactMain(struct Sprite *sprite)
-{
-    if (sprite->sActiveSprites == 0)
-    {
-        FreeSpriteTilesByTag(sSmokescreenImpactSpriteSheet.tag);
-        FreeSpritePaletteByTag(sSmokescreenImpactSpritePalette.tag);
-        if (!sprite->sPersist)
-            DestroySprite(sprite);
-        else
-            sprite->callback = SpriteCallbackDummy;
-    }
-}
-
-static void SpriteCB_SmokescreenImpact(struct Sprite *sprite)
-{
-    if (sprite->animEnded)
-    {
-        gSprites[sprite->sMainSpriteId].sActiveSprites--;
-        DestroySprite(sprite);
-    }
+    spriteId1 = CreateSprite(&sSmokescreenImpactSpriteTemplate, x, y, 2);
+    gSprites[spriteId1].sMainSpriteId = mainSpriteId;
+    gSprites[mainSpriteId].sActiveSprites++;
+    StartSpriteAnim(&gSprites[spriteId1], 3);
+    AnimateSprite(&gSprites[spriteId1]);
 }
