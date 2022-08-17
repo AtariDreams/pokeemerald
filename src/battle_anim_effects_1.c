615e8e3359267e9fdb5360de674bc261f35b5955
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-08-17 14:40:40 -0400

    MATCH

--- src/battle_anim_effects_1.c
+++ src/battle_anim_effects_1.c
@@ -8128,8 +8128,11 @@ void AnimTask_SketchDrawMon(u8 taskId)
 {
     struct Task* task = &gTasks[taskId];
     struct ScanlineEffectParams params;
-
+    #if !MODERN
     s16 i;
+    #else
+    int i;
+    #endif
     task->data[0] = GetBattlerYCoordWithElevation(gBattleAnimTarget) + 32;
     task->data[1] = 4;
     task->data[2] = 0;
