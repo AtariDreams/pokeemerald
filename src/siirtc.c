126f47d879a410f5995e2263468fb7a8cdba589a
Wiz <95232096+GameCubeGBA@users.noreply.github.com>
2022-08-17 14:40:51 -0400

    OK

--- src/siirtc.c
+++ src/siirtc.c
@@ -69,8 +69,14 @@ extern vu16 GPIOPortDirection;
 static u16 sDummy; // unused variable
 static bool8 sLocked;
 
+#ifndef UBFIX
 static int WriteCommand(u8 value);
 static int WriteData(u8 value);
+#else
+static void WriteCommand(u8 value);
+static void WriteData(u8 value);
+#endif
+
 static u8 ReadData();
 
 static void EnableGpioPortRead();
@@ -384,7 +390,11 @@ bool8 SiiRtcSetAlarm(struct SiiRtcInfo *rtc)
     return TRUE;
 }
 
+#ifndef UBFIX
 static int WriteCommand(u8 value)
+#else
+static void WriteCommand(u8 value)
+#endif
 {
     u8 i;
     u8 temp;
@@ -400,12 +410,13 @@ static int WriteCommand(u8 value)
 
     // Nothing uses the returned value from this function,
     // so the undefined behavior is harmless in the vanilla game.
-#ifdef UBFIX
-    return 0;
-#endif
 }
 
+#ifndef UBFIX
 static int WriteData(u8 value)
+#else
+static void WriteData(u8 value)
+#endif
 {
     u8 i;
     u8 temp;
@@ -421,9 +432,6 @@ static int WriteData(u8 value)
 
     // Nothing uses the returned value from this function,
     // so the undefined behavior is harmless in the vanilla game.
-#ifdef UBFIX
-    return 0;
-#endif
 }
 
 static u8 ReadData()
