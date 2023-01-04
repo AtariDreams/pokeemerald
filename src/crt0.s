5f435dce1b0b18af07b8aacbe3628f894b9b9b82
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-01-04 11:44:21 -0500

    Optimize crt0.s

--- src/crt0.s
+++ src/crt0.s
@@ -36,18 +36,18 @@ IntrMain::
 	mov r3, #REG_BASE
 	add r3, r3, #OFFSET_REG_IE
 	ldr r2, [r3]
-	ldrh r1, [r3, #OFFSET_REG_IME - 0x200]
+	ldrh r1, [r3, #REG_IME - REG_IE]
 	mrs r0, spsr
 	stmfd sp!, {r0-r3,lr}
 	mov r0, #0
-	strh r0, [r3, #OFFSET_REG_IME - 0x200]
+	strh r0, [r3, #REG_IME - REG_IE]
 	and r1, r2, r2, lsr #16
 	mov r12, #0
 	ands r0, r1, #INTR_FLAG_VCOUNT
 	bne IntrMain_FoundIntr
 	add r12, r12, 0x4
 	mov r0, 0x1
-	strh r0, [r3, #OFFSET_REG_IME - 0x200]
+	strh r0, [r3, #REG_IME - REG_IE]
 	ands r0, r1, #INTR_FLAG_SERIAL
 	bne IntrMain_FoundIntr
 	add r12, r12, 0x4
@@ -88,24 +88,24 @@ IntrMain::
 	strbne r0, [r3, #REG_SOUNDCNT_X - REG_IE]
 	bne . @ spin
 IntrMain_FoundIntr:
-	strh r0, [r3, #OFFSET_REG_IF - 0x200]
+	strh r0, [r3, #REG_IF - REG_IE]
 	bic r2, r2, r0
 	ldr r0, =gSTWIStatus
 	ldr r0, [r0]
 	ldrb r0, [r0, 0xA]
-	mov r1, 0x8
-	lsl r0, r1, r0
+	mov r1, #INTR_FLAG_TIMER0
+	lsl r0, r1, r0 @TIMER0_INTR_FLAG << STWI_status->timer_number
 	orr r0, r0, #INTR_FLAG_GAMEPAK
 	orr r1, r0, #INTR_FLAG_SERIAL | INTR_FLAG_TIMER3 | INTR_FLAG_VCOUNT | INTR_FLAG_HBLANK
 	and r1, r1, r2
-	strh r1, [r3, #OFFSET_REG_IE - 0x200]
+	strh r1, [r3, #REG_IE - REG_IE]
+
 	mrs r3, cpsr
 	bic r3, r3, #PSR_I_BIT | PSR_F_BIT | PSR_MODE_MASK
 	orr r3, r3, #PSR_SYS_MODE
 	msr cpsr_cf, r3
 	ldr r1, =gIntrTable
-	add r1, r1, r12
-	ldr r0, [r1]
+	ldr r0, [r1, r12]
 	stmfd sp!, {lr}
 	adr lr, IntrMain_RetAddr
 	bx r0
@@ -116,8 +116,8 @@ IntrMain_RetAddr:
 	orr r3, r3, #PSR_I_BIT | PSR_IRQ_MODE
 	msr cpsr_cf, r3
 	ldmia sp!, {r0-r3,lr}
-	strh r2, [r3, #OFFSET_REG_IE - 0x200]
-	strh r1, [r3, #OFFSET_REG_IME - 0x200]
+	strh r2, [r3, #REG_IE - REG_IE]
+	strh r1, [r3, #REG_IME - REG_IE]
 	msr spsr_cf, r0
 	bx lr
 
