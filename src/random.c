76d03dbc5468d67d9660c705a2fc6682273d8ac8
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-07-25 11:33:11 -0400

    random

--- src/random.c
+++ src/random.c
@@ -1,9 +1,6 @@
 #include "global.h"
 #include "random.h"
 
-EWRAM_DATA static u8 sUnknown = 0;
-EWRAM_DATA static u32 sRandCount = 0;
-
 // IWRAM common
 u32 gRngValue;
 u32 gRng2Value;
@@ -11,14 +8,12 @@ u32 gRng2Value;
 u16 Random(void)
 {
     gRngValue = ISO_RANDOMIZE1(gRngValue);
-    sRandCount++;
     return gRngValue >> 16;
 }
 
 void SeedRng(u16 seed)
 {
     gRngValue = seed;
-    sUnknown = 0;
 }
 
 void SeedRng2(u16 seed)
