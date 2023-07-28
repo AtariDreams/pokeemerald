d21ca78c339b4d280874254887199a0fbd5b2670
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-07-28 15:52:17 -0400

    Remove encryption stuff

--- include/berry_powder.h
+++ include/berry_powder.h
@@ -1,8 +1,6 @@
 #ifndef GUARD_BERRY_POWDER_H
 #define GUARD_BERRY_POWDER_H
 
-void SetBerryPowder(u32 *powder, u32 amount);
-void ApplyNewEncryptionKeyToBerryPowder(u32 encryptionKey);
 bool8 GiveBerryPowder(u32 amountToAdd);
 u32 GetBerryPowder(void);
 
