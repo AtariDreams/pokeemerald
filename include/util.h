c8add6b93ef32ae4a4a13766b75bfc12e472df21
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-08-01 15:41:17 -0400

    Finally, a compiler builtin

--- include/util.h
+++ include/util.h
@@ -7,9 +7,8 @@ extern const u8 gMiscBlank_Gfx[]; // unused in Emerald
 extern const u32 gBitTable[];
 
 u8 CreateInvisibleSpriteWithCallback(void (*)(struct Sprite *));
-void StoreWordInTwoHalfwords(u16 *, u32);
-void LoadWordFromTwoHalfwords(u16 *, u32 *);
-int CountTrailingZeroBits(u32 value);
+void StoreWordInTwoHalfwords(s16 *, void *);
+void LoadWordFromTwoHalfwords(s16 *, void *);
 u16 CalcCRC16(const u8 *data, s32 length);
 u16 CalcCRC16WithTable(const u8 *data, u32 length);
 u32 CalcByteArraySum(const u8 *data, u32 length);
