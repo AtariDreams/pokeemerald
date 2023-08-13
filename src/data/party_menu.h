fa6432dffe007b93c9f048818d556d165531eb90
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-08-13 13:48:44 -0400

    PokeParty

--- src/data/party_menu.h
+++ src/data/party_menu.h
@@ -34,30 +34,26 @@ static const struct PartyMenuBoxInfoRects sPartyBoxInfoRects[] =
     [PARTY_BOX_LEFT_COLUMN] =
     {
         BlitBitmapToPartyWindow_LeftColumn,
-        {
             //The below are the x, y, width, and height for each of the following info
-            24, 11, 40, 13, // Nickname
-            32, 20, 32,  8, // Level
-            64, 20,  8,  8, // Gender
-            38, 37, 24,  8, // HP
-            53, 37, 24,  8, // Max HP
-            24, 35, 48,  3  // HP bar
-        },
-        12, 34, 64, 16      // Description text (e.g. NO USE)
+            {24, 11, 40, 13}, // Nickname
+            {32, 20, 32,  8}, // Level
+            {64, 20,  8,  8}, // Gender
+            {38, 37, 24,  8}, // HP
+            {53, 37, 24,  8}, // Max HP
+            {24, 35, 48,  3}, // HP bar
+            {12, 34, 64, 16}, // Description text (e.g. NO USE)
     },
     [PARTY_BOX_RIGHT_COLUMN] =
     {
         BlitBitmapToPartyWindow_RightColumn,
-        {
              // See above comment
-             22,  3, 40, 13, // Nickname
-             30, 12, 32,  8, // Level
-             62, 12,  8,  8, // Gender
-            102, 12, 24,  8, // HP
-            117, 12, 24,  8, // Max HP
-             88, 10, 48,  3  // HP bar
-        },
-        77, 4, 64, 16        // Description text
+             {22,  3, 40, 13}, // Nickname
+             {30, 12, 32,  8}, // Level
+             {62, 12,  8,  8}, // Gender
+            {102, 12, 24,  8}, // HP
+            {117, 12, 24,  8}, // Max HP
+             {88, 10, 48,  3 }, // HP bar
+            {77, 4, 64, 16},       // Description text
     },
 };
 
