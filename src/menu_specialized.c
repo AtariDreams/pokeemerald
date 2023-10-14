d82603194601073fd7b90790256ccb0721cfd414
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-10-13 20:05:11 -0400

    Add failsafe for NONE

--- src/menu_specialized.c
+++ src/menu_specialized.c
@@ -806,7 +806,7 @@ static void MoveRelearnerLoadBattleMoveDescription(u32 chosenMove)
     }
     AddTextPrinterParameterized(RELEARNERWIN_DESC_BATTLE, FONT_NORMAL, str, 106, 41, TEXT_SKIP_DRAW, NULL);
 
-    str = gMoveDescriptionPointers[chosenMove - 1];
+    str = gMoveDescriptionPointers[chosenMove];
     AddTextPrinterParameterized(RELEARNERWIN_DESC_BATTLE, FONT_NARROW, str, 0, 65, 0, NULL);
 }
 
