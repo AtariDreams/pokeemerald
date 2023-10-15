e02935ffe48ed8ff1aea8fdae65597bef7e523c4
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-10-15 13:00:58 -0400

    get primer ball after buying 10 of any ball

--- src/shop.c
+++ src/shop.c
@@ -1149,8 +1149,8 @@ static void Task_ReturnToItemListAfterItemPurchase(u8 taskId)
     {
         PlaySE(SE_SELECT);
 
-        // Purchasing 10+ Poke Balls gets the player a Premier Ball
-        if (tItemId == ITEM_POKE_BALL && tItemCount >= 10 && AddBagItem(ITEM_PREMIER_BALL, 1) == TRUE)
+        // Purchasing 10+ Balls gets the player a Premier Ball
+        if (ITEM_ULTRA_BALL <= tItemId && tItemId <= ITEM_LUXURY_BALL && tItemCount >= 10 && AddBagItem(ITEM_PREMIER_BALL, 1) == TRUE)
             BuyMenuDisplayMessage(taskId, gText_ThrowInPremierBall, BuyMenuReturnToItemList);
         else
             BuyMenuReturnToItemList(taskId);
