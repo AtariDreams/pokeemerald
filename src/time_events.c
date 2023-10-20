7f91a4b03689310ff928d3c3ea6d15d60aaeb22e
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-10-20 01:43:58 -0400

    Count

--- src/time_events.c
+++ src/time_events.c
@@ -42,10 +42,10 @@ void UpdateMirageRnd(u16 days)
 bool8 IsMirageIslandPresent(void)
 {
     u16 rnd = GetMirageRnd() >> 16;
-    int i;
+    u32 i;
 
-    for (i = 0; i < PARTY_SIZE; i++)
-        if (GetMonData(&gPlayerParty[i], MON_DATA_SPECIES) && (GetMonData(&gPlayerParty[i], MON_DATA_PERSONALITY) & 0xFFFF) == rnd)
+    for (i = 0; i < gPlayerPartyCount; i++)
+        if ((GetMonData(&gPlayerParty[i], MON_DATA_PERSONALITY) & 0xFFFF) == rnd)
             return TRUE;
 
     return FALSE;
