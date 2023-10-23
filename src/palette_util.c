36e6a25ca9f91fdc0617f7ef48e6564c37d74832
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-10-23 19:43:37 -0400

    u32

--- src/palette_util.c
+++ src/palette_util.c
@@ -456,36 +456,3 @@ void SetTilemapRect(u16 *dest, const u16 *src, u8 left, u8 top, u8 width, u8 hei
             *_dest++ = *src++;
     }
 }
-
-static void UNUSED FillTilemapRect_Unused(void *dest, u16 value, u8 left, u8 top, u8 width, u8 height)
-{
-    u8 i, j;
-    u8 x, y;
-
-    for (i = 0, y = top; i < height; i++)
-    {
-        for (x = left, j = 0; j < width; j++)
-        {
-            *(u16 *)((dest) + (y * 64 + x * 2)) = value;
-            x = (x + 1) % 32;
-        }
-        y = (y + 1) % 32;
-    }
-}
-
-static void UNUSED SetTilemapRect_Unused(void *dest, const u16 *src, u8 left, u8 top, u8 width, u8 height)
-{
-    u8 i, j;
-    u8 x, y;
-    const u16 *_src;
-
-    for (i = 0, _src = src, y = top; i < height; i++)
-    {
-        for (x = left, j = 0; j < width; j++)
-        {
-            *(u16 *)((dest) + (y * 64 + x * 2)) = *(_src++);
-            x = (x + 1) % 32;
-        }
-        y = (y + 1) % 32;
-    }
-}
