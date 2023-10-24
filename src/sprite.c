44a8cae84573581a85b800d2e12480bb6d45f6fc
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-10-24 18:57:39 -0400

    ub

--- src/sprite.c
+++ src/sprite.c
@@ -827,10 +827,8 @@ void ResetAllSprites(void)
 void FreeSpriteTiles(struct Sprite *sprite)
 {
 // UB: template pointer may point to freed temporary storage
-#ifdef UBFIX
     if (!sprite || !sprite->template)
         return;
-#endif
 
     if (sprite->template->tileTag != TAG_NONE)
         FreeSpriteTilesByTag(sprite->template->tileTag);
@@ -839,10 +837,8 @@ void FreeSpriteTiles(struct Sprite *sprite)
 void FreeSpritePalette(struct Sprite *sprite)
 {
 // UB: template pointer may point to freed temporary storage
-#ifdef UBFIX
     if (!sprite || !sprite->template)
         return;
-#endif
 
     FreeSpritePaletteByTag(sprite->template->paletteTag);
 }
