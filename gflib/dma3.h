419a03edc99b7f29b90cccadaade657a0535ea9d
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-10-25 13:33:14 -0400

    Optimize DMA according to GBA spec

--- gflib/dma3.h
+++ gflib/dma3.h
@@ -1,50 +1,47 @@
 #ifndef GUARD_DMA3_H
 #define GUARD_DMA3_H
 
-// Maximum amount of data we will transfer in one operation
-#define MAX_DMA_BLOCK_SIZE 0x1000
-
-#define Dma3CopyLarge_(src, dest, size, bit)               \
+#define Dma3CopyLarge_(src, dest, size, blocksize, bit)               \
 {                                                          \
     const void *_src = src;                                \
     void *_dest = dest;                                    \
     u32 _size = size;                                      \
     while (1)                                              \
     {                                                      \
-        if (_size <= MAX_DMA_BLOCK_SIZE)                   \
+        if (_size <= blocksize)                   \
         {                                                  \
             DmaCopy##bit(3, _src, _dest, _size);           \
             break;                                         \
         }                                                  \
-        DmaCopy##bit(3, _src, _dest, MAX_DMA_BLOCK_SIZE);  \
-        _src += MAX_DMA_BLOCK_SIZE;                        \
-        _dest += MAX_DMA_BLOCK_SIZE;                       \
-        _size -= MAX_DMA_BLOCK_SIZE;                       \
+        DmaCopy##bit(3, _src, _dest, blocksize);  \
+        _src += blocksize;                        \
+        _dest += blocksize;                       \
+        _size -= blocksize;                       \
     }                                                      \
 }
 
-#define Dma3CopyLarge16_(src, dest, size) Dma3CopyLarge_(src, dest, size, 16)
-#define Dma3CopyLarge32_(src, dest, size) Dma3CopyLarge_(src, dest, size, 32)
+#define Dma3CopyLarge16_(src, dest, size) Dma3CopyLarge_(src, dest, size, 0x20000, 16)
+#define Dma3CopyLarge32_(src, dest, size) Dma3CopyLarge_(src, dest, size, 0x40000, 32)
 
-#define Dma3FillLarge_(value, dest, size, bit)             \
+#define Dma3FillLarge_(value, dest, size, blocksize, bit)             \
 {                                                          \
     void *_dest = dest;                                    \
     u32 _size = size;                                      \
     while (1)                                              \
     {                                                      \
-        if (_size <= MAX_DMA_BLOCK_SIZE)                   \
+        if (_size <= blocksize)                            \
         {                                                  \
             DmaFill##bit(3, value, _dest, _size);          \
             break;                                         \
         }                                                  \
-        DmaFill##bit(3, value, _dest, MAX_DMA_BLOCK_SIZE); \
-        _dest += MAX_DMA_BLOCK_SIZE;                       \
-        _size -= MAX_DMA_BLOCK_SIZE;                       \
+        DmaFill##bit(3, value, _dest, blocksize); \
+        _dest += blocksize;                       \
+        _size -= blocksize;                       \
     }                                                      \
 }
 
-#define Dma3FillLarge16_(value, dest, size) Dma3FillLarge_(value, dest, size, 16)
-#define Dma3FillLarge32_(value, dest, size) Dma3FillLarge_(value, dest, size, 32)
+#define Dma3FillLarge16_(value, dest, size) Dma3FillLarge_(value, dest, size, 0x20000, 16)
+#define Dma3FillLarge32_(value, dest, size) Dma3FillLarge_(value, dest, size, 0x40000, 32)
 
 void ClearDma3Requests(void);
 void ProcessDma3Requests(void);
