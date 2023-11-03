b6cb8ce12cd993849c7a206f0d8b2b6f9c5fab85
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-11-03 13:22:01 -0400

    battle transitions and trig

--- include/trig.h
+++ include/trig.h
@@ -3,9 +3,9 @@
 
 extern const s16 gSineTable[];
 
-s16 Sin(s16 index, s16 amplitude);
-s16 Cos(s16 index, s16 amplitude);
-s16 Sin2(u16 angle);
-s16 Cos2(u16 angle);
+s16 Sin(s16 index, s16 amplitude)  __attribute__ ((const));
+s16 Cos(s16 index, s16 amplitude)  __attribute__ ((const));
+s16 Sin2(u16 angle)  __attribute__ ((const));
+s16 Cos2(u16 angle)  __attribute__ ((const));;
 
 #endif // GUARD_TRIG_H
