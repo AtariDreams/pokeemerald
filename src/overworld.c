fd9c217809aadee9d625c053485bc194c2734305
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-11-03 16:05:51 -0400

    reorder

--- src/overworld.c
+++ src/overworld.c
@@ -1743,8 +1743,8 @@ static void FieldClearVBlankHBlankCallbacks(void)
 
     if (gWirelessCommType != 0)
     {
-        EnableInterrupts(INTR_FLAG_VBLANK | INTR_FLAG_VCOUNT | INTR_FLAG_TIMER3 | INTR_FLAG_SERIAL);
         DisableInterrupts(INTR_FLAG_HBLANK);
+        EnableInterrupts(INTR_FLAG_VBLANK | INTR_FLAG_VCOUNT | INTR_FLAG_TIMER3 | INTR_FLAG_SERIAL);
     }
     else
     {
