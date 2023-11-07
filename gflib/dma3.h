6ced98e87dc3a38c7a0343a6da7320d5e8358b54
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-11-07 09:49:01 -0500

    DMA

--- gflib/dma3.h
+++ gflib/dma3.h
@@ -45,7 +45,7 @@
 
 void ClearDma3Requests(void);
 void ProcessDma3Requests(void);
-s16 RequestDma3Copy(const void *src, void *dest, u16 size, u8 mode);
+int RequestDma3Copy(const void *src, void *dest, u16 size, u8 mode);
 s16 RequestDma3Fill(u32 value, void *dest, u16 size, u8 mode);
 s16 CheckForSpaceForDma3Request(s16 index);
 
