4334b1387f77c37309310ab02ec405a5ddabcaa4
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-11-09 15:37:01 -0500

    crt0 hack

--- src/crt0.s
+++ src/crt0.s
@@ -1,18 +1,16 @@
-	.include "constants/gba_constants.inc"
-
 	.syntax unified
 
 	.arm
 
 	.align 2, 0
 Init::
-	mov r0, #PSR_IRQ_MODE
+	mov r0, #0x12
 	msr cpsr_cf, r0
 	ldr sp, sp_irq
-	mov r0, #PSR_SYS_MODE
+	mov r0, #0x1f
 	msr cpsr_cf, r0
 	ldr sp, sp_sys
-	ldr r1, =INTR_VECTOR
+	ldr r1, =0x3007FFC
 	adr r0, IntrMain
 	str r0, [r1]
 	mov r0, #255 @ RESET_ALL
@@ -30,22 +28,22 @@ sp_irq: .word IWRAM_END - 0x60
 	.arm
 	.align 2, 0
 IntrMain::
-	mov r3, #REG_BASE
-	add r3, r3, #OFFSET_REG_IE
+	mov r3, #0x4000000
+	add r3, r3, #0x200
 	ldr r2, [r3]
-	ldrh r1, [r3, #REG_IME - REG_IE]
+	ldrh r1, [r3, #0x8]
 	mrs r0, spsr
 	push {r0-r3,lr}
 	mov r0, #0
-	strh r0, [r3, #REG_IME - REG_IE]
+	strh r0, [r3, #0x8]
 	and r1, r2, r2, lsr #16
 	mov r12, #0
-	ands r0, r1, #INTR_FLAG_VCOUNT
+	ands r0, r1, #0x4
 	bne IntrMain_FoundIntr
 	add r12, r12, 0x4
 	mov r0, 0x1
-	strh r0, [r3, #REG_IME - REG_IE]
-	ands r0, r1, #INTR_FLAG_SERIAL
+	strh r0, [r3, #0x8]
+	ands r0, r1, #0x80
 	bne IntrMain_FoundIntr
 	add r12, r12, 0x4
 	ands r0, r1, #INTR_FLAG_TIMER3
@@ -81,7 +79,7 @@ IntrMain::
 	ands r0, r1, #INTR_FLAG_KEYPAD
 	bne IntrMain_FoundIntr
 	add r12, r12, 0x4
-	ands r0, r1, #INTR_FLAG_GAMEPAK
+	ands r0, r1, #1 << 13
 	strbne r0, [r3, #REG_SOUNDCNT_X - REG_IE]
 	bne . @ spin
 IntrMain_FoundIntr:
@@ -92,13 +90,13 @@ IntrMain_FoundIntr:
 	ldrb r0, [r0, 0xA]
 	mov r1, #INTR_FLAG_TIMER0
 	lsl r0, r1, r0
-	orr r0, r0, #INTR_FLAG_GAMEPAK
-	orr r1, r0, #INTR_FLAG_SERIAL | INTR_FLAG_TIMER3 | INTR_FLAG_VCOUNT | INTR_FLAG_HBLANK
+	orr r0, r0, #1 << 13
+	orr r1, r0, #0xc6
 	and r1, r1, r2
 	strh r1, [r3]
 	mrs r3, cpsr
-	bic r3, r3, #PSR_I_BIT | PSR_F_BIT | PSR_MODE_MASK
-	orr r3, r3, #PSR_SYS_MODE
+	bic r3, r3, #0xdf
+	orr r3, r3, #0x1f
 	msr cpsr_cf, r3
 	ldr r1, =gIntrTable
 	ldr r0, [r1, r12]
@@ -108,12 +106,12 @@ IntrMain_FoundIntr:
 IntrMain_RetAddr:
 	pop {lr}
 	mrs r3, cpsr
-	bic r3, r3, #PSR_I_BIT | PSR_F_BIT | PSR_MODE_MASK
-	orr r3, r3, #PSR_I_BIT | PSR_IRQ_MODE
+	bic r3, r3, #0xdf
+	orr r3, r3, #0xx92
 	msr cpsr_cf, r3
 	pop {r0-r3,lr}
 	strh r2, [r3]
-	strh r1, [r3, #REG_IME - REG_IE]
+	strh r1, [r3, #0x8]
 	msr spsr_cf, r0
 	bx lr
 
