f537bd1988167f64ebf3e71e1f0c2f9c980d09f5
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-11-10 12:58:44 -0500

    agbflash

--- include/gba/flash_internal.h
+++ include/gba/flash_internal.h
@@ -1,9 +1,34 @@
 #ifndef GUARD_GBA_FLASH_INTERNAL_H
 #define GUARD_GBA_FLASH_INTERNAL_H
 
-#define FLASH_BASE ((u8 *)0xE000000)
 
-#define FLASH_WRITE(addr, data) ((*(vu8 *)(FLASH_BASE + (addr))) = (data))
+#define FLASH_ADR         0xE000000
+#define PHASE_VERIFY			0x0000
+#define	PHASE_PROGRAM			0x0001
+#define PHASE_SECTOR_ERASE		0x0002
+#define PHASE_CHIP_ERASE		0x0003
+#define	PHASE_VERIFY_ERASE		0x0004
+#define	PHASE_PARAMETER_CHECK	0x00ff
+
+#define	RESULT_OK				0x0000
+#define	RESULT_ERROR			0x8000
+#define	RESULT_TIMEOUT			0x4000
+#define RESULT_Q5TIMEOUT		0x2000
+
+
+#define	VENDER_ID_ADR		(FLASH_ADR+0x00000000)
+#define	DEVICE_ID_ADR		(FLASH_ADR+0x00000001)
+#define	COM_ADR1			(FLASH_ADR+0x00005555)
+#define COM_ADR2			(FLASH_ADR+0x00002aaa)
+
+#define	MEGA_512K_ID		0x1cc2
+#define	MEGA_1M_ID			0x09c2
+
+
+#define FLASH_BASE         (vu8*)0xE000000
+
+
+#define FLASH_WRITE(addr, data) ((*(vu8 *)(FLASH_ADR + (addr))) = (data))
 
 #define FLASH_ROM_SIZE_1M 131072 // 1 megabit ROM
 
@@ -39,7 +64,7 @@ struct FlashSetupInfo
     u16 (*eraseFlashChip)(void);
     u16 (*eraseFlashSector)(u16);
     u16 (*WaitForFlashWrite)(u8, vu8 *, u8);
-    const u16 *maxTime;
+    const u16 (*maxTime)[3];
     struct FlashType type;
 };
 
@@ -50,7 +75,7 @@ extern u16 (*ProgramFlashSector)(u16, vu8 *);
 extern u16 (*EraseFlashChip)(void);
 extern u16 (*EraseFlashSector)(u16);
 extern u16 (*WaitForFlashWrite)(u8, vu8 *, u8);
-extern const u16 *gFlashMaxTime;
+extern const u16 (*gFlashMaxTime)[3];
 extern const struct FlashType *gFlash;
 
 extern u8 (*PollFlashStatus)(vu8 *);
