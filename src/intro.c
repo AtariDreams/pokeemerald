0689a96527df43540aad93f9ba473b90a95b3f55
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-11-12 13:43:50 -0500

    Remove gamecube boot

--- src/intro.c
+++ src/intro.c
@@ -4,7 +4,6 @@
 #include "scanline_effect.h"
 #include "task.h"
 #include "title_screen.h"
-#include "libgcnmultiboot.h"
 #include "malloc.h"
 #include "gpu_regs.h"
 #include "link.h"
@@ -173,7 +172,6 @@ static EWRAM_DATA u16 UNUSED sUnusedVar = 0;
 static EWRAM_DATA u16 sFlygonYOffset = 0;
 
 u32 gIntroFrameCounter;
-struct GcmbStruct gMultibootProgramStruct;
 
 static const u16 sIntroDrops_Pal[]            = INCBIN_U16("graphics/intro/scene_1/drops.gbapal");
 static const u16 sIntroLogo_Pal[]             = INCBIN_U16("graphics/intro/scene_1/logo.gbapal");
@@ -1056,12 +1054,7 @@ static void LoadCopyrightGraphics(u16 tilesetAddress, u16 tilemapAddress, u16 pa
     LZ77UnCompVram(gIntroCopyright_Gfx, (void *)(VRAM + tilesetAddress));
     LZ77UnCompVram(gIntroCopyright_Tilemap, (void *)(VRAM + tilemapAddress));
     LoadPalette(gIntroCopyright_Pal, paletteOffset, PLTT_SIZE_4BPP);
-}
-
-static void SerialCB_CopyrightScreen(void)
-{
-    GameCubeMultiBoot_HandleSerialInterrupt(&gMultibootProgramStruct);
-}
+} 
 
 static u8 SetUpCopyrightScreen(void)
 {
@@ -1095,26 +1088,27 @@ static u8 SetUpCopyrightScreen(void)
         SetVBlankCallback(VBlankCB_Intro);
         SetGpuReg(REG_OFFSET_DISPCNT,DISPCNT_MODE_0 | DISPCNT_OBJ_1D_MAP | DISPCNT_BG0_ON);
         // REG_DISPCNT = DISPCNT_MODE_0 | DISPCNT_OBJ_1D_MAP | DISPCNT_BG0_ON;
+#if 0
         SetSerialCallback(SerialCB_CopyrightScreen);
         GameCubeMultiBoot_Init(&gMultibootProgramStruct);
+#endif
     default:
         UpdatePaletteFade();
         gMain.state++;
-        GameCubeMultiBoot_Main(&gMultibootProgramStruct);
+        //GameCubeMultiBoot_Main(&gMultibootProgramStruct);
         break;
     case 140:
-        GameCubeMultiBoot_Main(&gMultibootProgramStruct);
-        if (gMultibootProgramStruct.gcmb_field_2 != 1)
-        {
-            BeginNormalPaletteFade(PALETTES_ALL, 0, 0, 16, RGB_BLACK);
-            gMain.state++;
-        }
+       // GameCubeMultiBoot_Main(&gMultibootProgramStruct);
+        //if (gMultibootProgramStruct.gcmb_field_2 == 1) break;
+        BeginNormalPaletteFade(PALETTES_ALL, 0, 0, 16, RGB_BLACK);
+        gMain.state++;
         break;
     case 141:
         if (UpdatePaletteFade())
             break;
         CreateTask(Task_Scene1_Load, 0);
         SetMainCallback2(MainCB2_Intro);
+        #if 0
         if (gMultibootProgramStruct.gcmb_field_2 != 0)
         {
             if (gMultibootProgramStruct.gcmb_field_2 == 2)
@@ -1133,6 +1127,7 @@ static u8 SetUpCopyrightScreen(void)
             GameCubeMultiBoot_Quit();
             SetSerialCallback(SerialCB);
         }
+        #endif
         return 0;
     }
 
