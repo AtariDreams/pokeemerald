619298ca9ba14266fa5e13b9d37f12c703c151c7
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-11-25 18:05:47 -0500

    Fixup contest

--- include/battle.h
+++ include/battle.h
@@ -630,7 +630,7 @@ extern u8 gBattleTextBuff3[TEXT_BUFF_ARRAY_COUNT];
 extern u32 gBattleTypeFlags;
 extern u8 gBattleTerrain;
 extern u8 *gBattleAnimBgTileBuffer;
-extern u8 *gBattleAnimBgTilemapBuffer;
+extern u16 *gBattleAnimBgTilemapBuffer;
 extern u8 gBattleBufferA[MAX_BATTLERS_COUNT][0x200];
 extern u8 gBattleBufferB[MAX_BATTLERS_COUNT][0x200];
 extern u8 gActiveBattler;
