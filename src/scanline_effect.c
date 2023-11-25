229d383372f0caff14860bc5f824556a65f522d8
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-11-25 12:52:43 -0500

    ok

--- src/scanline_effect.c
+++ src/scanline_effect.c
@@ -74,8 +74,8 @@ void ScanlineEffect_InitHBlankDmaTransfer(void)
     if (gScanlineEffect.state == 3)
     {
         sShouldStopWaveTask = TRUE;
-        DmaStop(0);
         gScanlineEffect.state = 0;
+        DmaStop(0);
         return;
     }
 
