df7cab0398f231f6b00079f9e54a601a60e767a9
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-11-30 16:31:59 -0500

    Optimize pokedex?

--- src/pokedex_area_region_map.c
+++ src/pokedex_area_region_map.c
@@ -19,24 +19,24 @@ void LoadPokedexAreaMapGfx(const struct PokedexAreaMapTemplate *template)
     u8 mode;
     void * tilemap;
     sPokedexAreaMapBgNum = Alloc(sizeof(sPokedexAreaMapBgNum));
-    mode = template->mode;
+   // mode = template->mode;
 
-    if (mode == 0)
+    //if (mode == 0)
     {
         SetBgAttribute(template->bg, BG_ATTR_METRIC, 0);
         DecompressAndCopyTileDataToVram(template->bg, sPokedexAreaMap_Gfx, 0, template->offset, 0);
         tilemap = DecompressAndCopyTileDataToVram(template->bg, sPokedexAreaMap_Tilemap, 0, 0, 1);
-        AddValToTilemapBuffer(tilemap, template->offset, 32, 32, FALSE); // template->offset is always 0, so this does nothing.
+        //AddValToTilemapBuffer(tilemap, template->offset, 32, 32, FALSE); // template->offset is always 0, so this does nothing.
     }
-    else
-    {
-        // This is never reached, only a mode of 0 is given
-        SetBgAttribute(template->bg, BG_ATTR_METRIC, 2);
-        SetBgAttribute(template->bg, BG_ATTR_TYPE, BG_TYPE_AFFINE); // This does nothing. BG_ATTR_TYPE can't be set with this function
-        DecompressAndCopyTileDataToVram(template->bg, sPokedexAreaMapAffine_Gfx, 0, template->offset, 0);
-        tilemap = DecompressAndCopyTileDataToVram(template->bg, sPokedexAreaMapAffine_Tilemap, 0, 0, 1);
-        AddValToTilemapBuffer(tilemap, template->offset, 64, 64, TRUE); // template->offset is always 0, so this does nothing.
-    }
+    // else
+    // {
+    //     // This is never reached, only a mode of 0 is given
+    //     SetBgAttribute(template->bg, BG_ATTR_METRIC, 2);
+    //     SetBgAttribute(template->bg, BG_ATTR_TYPE, BG_TYPE_AFFINE); // This does nothing. BG_ATTR_TYPE can't be set with this function
+    //     DecompressAndCopyTileDataToVram(template->bg, sPokedexAreaMapAffine_Gfx, 0, template->offset, 0);
+    //     tilemap = DecompressAndCopyTileDataToVram(template->bg, sPokedexAreaMapAffine_Tilemap, 0, 0, 1);
+    //     //AddValToTilemapBuffer(tilemap, template->offset, 64, 64, TRUE); // template->offset is always 0, so this does nothing.
+    // }
 
     ChangeBgX(template->bg, 0, BG_COORD_SET);
     ChangeBgY(template->bg, 0, BG_COORD_SET);
